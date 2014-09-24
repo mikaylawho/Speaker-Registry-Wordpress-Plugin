1eb5b63ea546683b280d6e923c0d450f2bc58349
Mikel Hensley <mikel.i.hensley@gmail.com>
2014-09-24 17:45:19 -0400

    New metabox to select a speaker for an event. Lots of copy/paste--need to refactor.

--- wp-content/plugins/kyss-convention-scheduler/kyss-convention-scheduler.php
+++ wp-content/plugins/kyss-convention-scheduler/kyss-convention-scheduler.php
@@ -60,33 +60,33 @@ if ( ! function_exists('kyss_con_speakers_post_type') ) {
 // Register Custom Post Type
 	function kyss_con_speakers_post_type() {
 
-		$labels = array(
-			'name'                => _x( 'Scheduled Speakers', 'Post Type General Name', 'text_domain' ),
-			'singular_name'       => _x( 'Scheduled Speaker', 'Post Type Singular Name', 'text_domain' ),
-			'menu_name'           => __( 'Scheduled Speakers', 'text_domain' ),
-			'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
-			'all_items'           => __( 'All Scheduled Speakers', 'text_domain' ),
-			'view_item'           => __( 'View Scheduled Speaker', 'text_domain' ),
-			'add_new_item'        => __( 'Schedule New Speaker', 'text_domain' ),
-			'add_new'             => __( 'Schedule Speaker', 'text_domain' ),
-			'edit_item'           => __( 'Edit Scheduled Speaker', 'text_domain' ),
-			'update_item'         => __( 'Update Scheduled Speaker', 'text_domain' ),
-			'search_items'        => __( 'Search Scheduled Speakers', 'text_domain' ),
-			'not_found'           => __( 'Not found', 'text_domain' ),
-			'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+		$labels  = array(
+			'name'               => _x( 'Scheduled Speakers', 'Post Type General Name', 'text_domain' ),
+			'singular_name'      => _x( 'Scheduled Speaker', 'Post Type Singular Name', 'text_domain' ),
+			'menu_name'          => __( 'Scheduled Speakers', 'text_domain' ),
+			'parent_item_colon'  => __( 'Parent Item:', 'text_domain' ),
+			'all_items'          => __( 'All Scheduled Speakers', 'text_domain' ),
+			'view_item'          => __( 'View Scheduled Speaker', 'text_domain' ),
+			'add_new_item'       => __( 'Schedule New Speaker', 'text_domain' ),
+			'add_new'            => __( 'Schedule Speaker', 'text_domain' ),
+			'edit_item'          => __( 'Edit Scheduled Speaker', 'text_domain' ),
+			'update_item'        => __( 'Update Scheduled Speaker', 'text_domain' ),
+			'search_items'       => __( 'Search Scheduled Speakers', 'text_domain' ),
+			'not_found'          => __( 'Not found', 'text_domain' ),
+			'not_found_in_trash' => __( 'Not found in Trash', 'text_domain' ),
 		);
 		$rewrite = array(
-			'slug'                => 'kyss_con_speakers',
-			'with_front'          => true,
-			'pages'               => true,
-			'feeds'               => true,
+			'slug'       => 'kyss_con_speakers',
+			'with_front' => true,
+			'pages'      => true,
+			'feeds'      => true,
 		);
-		$args = array(
+		$args    = array(
 			'label'               => __( 'con_speaker', 'text_domain' ),
 			'description'         => __( 'Scheduled Speakers', 'text_domain' ),
 			'labels'              => $labels,
 			'supports'            => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields', ),
-			'taxonomies'          => array( 'Category'),
+			'taxonomies'          => array( 'Category' ),
 			'hierarchical'        => false,
 			'public'              => true,
 			'show_ui'             => true,
@@ -110,7 +110,6 @@ if ( ! function_exists('kyss_con_speakers_post_type') ) {
 	add_action( 'init', 'kyss_con_speakers_post_type', 0 );
 
 }
-
 
 //register custom post type for speakers
 if ( ! function_exists('kyss_con_event_post_type') ) {
@@ -173,7 +172,7 @@ function kyss_render_con_event_info_metabox( $post ) {
 
 	?>
 
-<!--	TODO: Set fields in the metabox for start and end times. Include only the start date field (for possible multi-day convention.)-->
+<!--TODO: Set fields in the metabox for start and end times. Include only the start date field (for possible multi-day convention.)-->
 
 	<label for="kyss-con-event-start-date"><?php _e( 'Event Start Date:', 'kyss' ); ?></label>
 	<input class="widefat kyss-con-event-date-input" id="kyss-con-event-start-date" type="text" name="kyss-con-event-start-date" placeholder="Format: February 18, 2014" value="<?php echo date( 'F d, Y', $event_start_date ); ?>" />
@@ -232,6 +231,76 @@ function kyss_save_event_info( $post_id ) {
 	}
 }
 add_action( 'save_post', 'kyss_save_event_info' );
+
+///*
+//// Create the meta box
+//function select_speaker_box_add_meta_box() {
+//	add_meta_box(
+//		'kyss-event-speaker',
+//		__( 'Event Speaker', 'kyss' ),
+//		'speaker_select_box_content',
+//		'event',
+//		'side',
+//		'core'
+//	);
+//}
+//
+//// Create the meta box content
+//function speaker_select_box_content() {
+//
+//	// generate a nonce field
+//	wp_nonce_field( basename( __FILE__ ), 'kyss-con-event-info-nonce' );
+//
+//	$static_args = array(
+//		'post_type' => 'con_speaker',
+//		'show_option_none' => 'Choose Speaker'
+//	);
+//
+//	$args = array( 'posts_per_page' => -1, 'orderby'=> 'title', 'order' => 'ASC', 'post_type' => 'speaker' );
+//
+////	$speakers = get_posts($args);
+//
+////	foreach( $speakers as $post ) : setup_postdata($post);
+//		$selected_item = get_post_meta($post->ID,'_selected_item', true);
+//		echo $post->title;
+//		for($i=0; $i<=2; $i++) {
+//			$incrementing_args = array(
+//				'id' => "selected_item_{$i}",
+//				'name' => 'selected_item[]',
+//				'selected' => (empty($selected_item[$i]) ? 0 : $selected_item[$i]),
+//			);
+//		}
+//
+//		wp_dropdown_pages(array_merge($static_args,$incrementing_args));
+////	endforeach;
+//
+//}
+//
+//// Save the selection
+//function speaker_select_box_save_postdata($data, $postarr) {
+//	// checking if the post being saved is an 'event',
+//	// if not, then return
+//	if ( 'event' != $_POST['post_type'] ) {
+//		return;
+//	}
+//
+//	// checking for the 'save' status
+//	$is_autosave = wp_is_post_autosave( $post_id );
+//	$is_revision = wp_is_post_revision( $post_id );
+//	$is_valid_nonce = ( isset( $_POST['kyss-event-info-nonce'] ) && ( wp_verify_nonce( $_POST['kyss-event-info-nonce'], basename( __FILE__ ) ) ) ) ? true : false;
+//
+//	// exit depending on the save status or if the nonce is not valid
+//	if ( $is_autosave || $is_revision || ! $is_valid_nonce ) {
+//		return;
+//	}
+//
+//	update_post_meta($postarr['ID'], '_selected_item', $postarr['selected_item']);
+//	return $data;
+//}
+//
+//
+//add_action( 'add_meta_boxes', 'select_speaker_box_add_meta_box' );
+//add_action( 'save_post', 'speaker_select_box_save_postdata' );*/
 
 
 
@@ -310,6 +379,8 @@ class ConventionSchedulerSettingsPage
 			'kyss_event_page_settings' // Section
 		);
 
+		/* TODO: Add fields for Convention Title, Convention Description, start date, end date, start time, end time.*/
+
 	}
 
 	/**
@@ -320,11 +391,11 @@ class ConventionSchedulerSettingsPage
 	public function sanitize( $input )
 	{
 		$new_input = array();
-		if( isset( $input['id_number'] ) )
-			$new_input['id_number'] = absint( $input['id_number'] );
-
-		if( isset( $input['title'] ) )
-			$new_input['title'] = sanitize_text_field( $input['title'] );
+//		if( isset( $input['id_number'] ) )
+//			$new_input['id_number'] = absint( $input['id_number'] );
+//
+//		if( isset( $input['title'] ) )
+//			$new_input['title'] = sanitize_text_field( $input['title'] );
 
 		return $new_input;
 	}
@@ -353,3 +424,63 @@ if( is_admin() )
 	$my_settings_page = new ConventionSchedulerSettingsPage();
 
 /* End configuration screen */
+
+
+/**
+ * Plugin Name: @WPSE 85107
+ * Description: <a target="_blank" href="http://wordpress.stackexchange.com/q/85107/89">WPSE 85107</a>
+ */
+
+class WPSE_85107 {
+	var $FOR_POST_TYPE = 'event';
+	var $SELECT_POST_TYPE = 'con_speaker';
+	var $SELECT_POST_LABEL = 'Speaker';
+	var $box_id;
+	var $box_label;
+	var $field_id;
+	var $field_label;
+	var $field_name;
+	var $meta_key;
+	function __construct() {
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+	}
+	function admin_init() {
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+		add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+		$this->meta_key     = "_selected_{$this->SELECT_POST_TYPE}";
+		$this->box_id       = "select-{$this->SELECT_POST_TYPE}-metabox";
+		$this->field_id     = "selected-{$this->SELECT_POST_TYPE}";
+		$this->field_name   = "selected_{$this->SELECT_POST_TYPE}";
+		$this->box_label    = __( "Select {$this->SELECT_POST_LABEL}", 'wpse-85107' );
+		$this->field_label  = __( "Choose {$this->SELECT_POST_LABEL}", 'wpse-85107' );
+	}
+	function add_meta_boxes() {
+		add_meta_box(
+			$this->box_id,
+			$this->box_label,
+			array( $this, 'select_box' ),
+			$this->FOR_POST_TYPE,
+			'side'
+		);
+	}
+	function select_box( $post ) {
+		$selected_post_id = get_post_meta( $post->ID, $this->meta_key, true );
+		global $wp_post_types;
+		$save_hierarchical = $wp_post_types[$this->SELECT_POST_TYPE]->hierarchical;
+		$wp_post_types[$this->SELECT_POST_TYPE]->hierarchical = true;
+		wp_dropdown_pages( array(
+			'id' => $this->field_id,
+			'name' => $this->field_name,
+			'selected' => empty( $selected_post_id ) ? 0 : $selected_post_id,
+			'post_type' => $this->SELECT_POST_TYPE,
+			'show_option_none' => $this->field_label,
+		));
+		$wp_post_types[$this->SELECT_POST_TYPE]->hierarchical = $save_hierarchical;
+	}
+	function save_post( $post_id, $post ) {
+		if ( $post->post_type == $this->FOR_POST_TYPE && isset( $_POST[$this->field_name] ) ) {
+			update_post_meta( $post_id, $this->meta_key, $_POST[$this->field_name] );
+		}
+	}
+}
+new WPSE_85107();
