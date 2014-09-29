27ea372be5281e4a9d0027e100455d3fdf397344
Mikel Hensley <mikel.i.hensley@gmail.com>
2014-09-29 15:41:19 -0400

    Syncing of CiviCrm members into Wordpress is now fully functional. For unknown reasons, the original functionality is pulling the emails addresses of the new WP users back into Civicrm as new contacts with the email address set as their name. Will need to troubleshoot this later.

--- wp-content/plugins/civi_member_sync/list.php
+++ wp-content/plugins/civi_member_sync/list.php
@@ -1,60 +1,63 @@
-<?php 
-	require_once('civi.php');
-	global $wpdb;
-    if(isset($_GET['q']) && $_GET['q'] == "delete" ) {
-	    if ( ! empty( $_GET['id'] ) ) {
-		    $wpdb->civi_member_sync = $wpdb->prefix . 'civi_member_sync';
-		    $rid                    = $_GET['id'];
-		    $delete                 = $wpdb->get_results( $wpdb->prepare( "DELETE FROM $wpdb->civi_member_sync WHERE id = %d", $rid ) );
-	    }
-    }
-$addNew_url = get_bloginfo('url')."/wp-admin/admin.php?&page=civi_member_sync/settings.php"; 
-$manual_sync_url = get_bloginfo('url')."/wp-admin/admin.php?&page=civi_member_sync/manual_sync.php";  
+<?php
+require_once( 'civi.php' );
+global $wpdb;
+if ( isset( $_GET['q'] ) && $_GET['q'] == "delete" ) {
+	if ( ! empty( $_GET['id'] ) ) {
+		$wpdb->civi_member_sync = $wpdb->prefix . 'civi_member_sync';
+		$rid                    = $_GET['id'];
+		$delete                 = $wpdb->get_results( $wpdb->prepare( "DELETE FROM $wpdb->civi_member_sync WHERE id = %d", $rid ) );
+	}
+}
+$addNew_url      = get_bloginfo( 'url' ) . "/wp-admin/admin.php?&page=civi_member_sync/settings.php";
+$manual_sync_url = get_bloginfo( 'url' ) . "/wp-admin/admin.php?&page=civi_member_sync/manual_sync.php";
 ?>
 <div id="icon-edit-pages" class="icon32"></div>
-<div class="wrap">     
-    <h2>LIST ASSOCIATION RULE(S)<a class="add-new-h2" href=<?php echo $addNew_url ?>>Add Association Rule</a><a class="add-new-h2" href=<?php echo $manual_sync_url ?>>Manually Synchronize</a></h2>
+<div class="wrap">
+	<h2>LIST ASSOCIATION RULE(S)<a class="add-new-h2" href=<?php echo $addNew_url ?>>Add Association Rule</a><a
+			class="add-new-h2" href=<?php echo $manual_sync_url ?>>Manually Synchronize</a></h2>
 </div>
 
 <?php
 $tablename = $wpdb->prefix . 'civi_member_sync';
 //changed to %s from $tablename. The syntax was wrong for string replacement...
 //replaced "Select *" with column names
-$select = $wpdb->get_results(" SELECT id, wp_role, civi_mem_type, current_rule, expiry_rule, expire_wp_role FROM " . $tablename );
+$select = $wpdb->get_results( " SELECT id, wp_role, civi_mem_type, current_rule, expiry_rule, expire_wp_role FROM " . $tablename );
 
 ?>
 <table cellspacing="0" border="0" class="wp-list-table widefat fixed users">
- <thead>
-    <tr>
-        <th style="" class="manage-column column-role" id="role" scope="col">Civi Membership Type</th>
-        <th style="" class="manage-column column-role" id="role" scope="col">Wordpress Role</th>
-        <th style="" class="manage-column column-role" id="role" scope="col">Current Codes</th>
-        <th style="" class="manage-column column-role" id="role" scope="col">Expired Codes</th>
-        <th style="" class="manage-column column-role" id="role" scope="col">Expiry Assign Role</th>
-    </tr>
- </thead>    
- <tbody class="list:civimember-role-sync" id="the-list">
- <?php if ( isset( $select ) ) {
-	 foreach ($select as $key => $value){?>
-	 <tr>
-	  <td><?php global $MembershipType;
-		  echo get_names($value->civi_mem_type, $MembershipType); ?>
-	  <br />
-	  <?php $edit_url = get_site_url() ."/wp-admin/admin.php?&q=edit&id=".$value->id."&page=civi_member_sync/settings.php";  ?>
-	  <?php $delete_url = get_site_url() ."/wp-admin/admin.php?&q=delete&id=".$value->id."&page=civi_member_sync/list.php";  ?>
-	  <div class="row-actions">
+	<thead>
+	<tr>
+		<th style="" class="manage-column column-role" id="role" scope="col">Civi Membership Type</th>
+		<th style="" class="manage-column column-role" id="role" scope="col">Wordpress Role</th>
+		<th style="" class="manage-column column-role" id="role" scope="col">Current Codes</th>
+		<th style="" class="manage-column column-role" id="role" scope="col">Expired Codes</th>
+		<th style="" class="manage-column column-role" id="role" scope="col">Expiry Assign Role</th>
+	</tr>
+	</thead>
+	<tbody class="list:civimember-role-sync" id="the-list">
+	<?php if ( isset( $select ) ) {
+		foreach ( $select as $key => $value ) {
+			?>
+			<tr>
+				<td><?php global $MembershipType;
+					echo get_names( $value->civi_mem_type, $MembershipType ); ?>
+					<br/>
+					<?php $edit_url = get_site_url() . "/wp-admin/admin.php?&q=edit&id=" . $value->id . "&page=civi_member_sync/settings.php"; ?>
+					<?php $delete_url = get_site_url() . "/wp-admin/admin.php?&q=delete&id=" . $value->id . "&page=civi_member_sync/list.php"; ?>
+					<div class="row-actions">
 	      <span class="edit">
 	      <a href="<?php echo $edit_url ?>">Edit</a> | </span>
-	      <span class="delete"><a href="<?php echo $delete_url ?>" class="submitdelete">Delete</a></span>
-	  </div>
-	  </td>
-	  <td><?php echo $value->wp_role; ?></td>
-	  <td><?php global $MembershipStatus;
-		  echo get_names_serialized($value->current_rule, $MembershipStatus); ?></td>
-	  <td><?php echo get_names_serialized($value->expiry_rule, $MembershipStatus);?></td>
-	  <td><?php echo $value->expire_wp_role; ?></td>
-	  </tr>
-	  <?php }
- } ?>
-  </tbody>
+						<span class="delete"><a href="<?php echo $delete_url ?>" class="submitdelete">Delete</a></span>
+					</div>
+				</td>
+				<td><?php echo $value->wp_role; ?></td>
+				<td><?php global $MembershipStatus;
+					echo get_names_serialized( $value->current_rule, $MembershipStatus ); ?></td>
+				<td><?php echo get_names_serialized( $value->expiry_rule, $MembershipStatus ); ?></td>
+				<td><?php echo $value->expire_wp_role; ?></td>
+			</tr>
+		<?php
+		}
+	} ?>
+	</tbody>
 </table>  
--- wp-content/plugins/civi_member_sync/manual_sync.php
+++ wp-content/plugins/civi_member_sync/manual_sync.php
@@ -1,46 +1,53 @@
 <div id="icon-edit-pages" class="icon32"></div>
 <div class="wrap">
- <h2 id="add-new-user">Manual Synchronize</h2>
+	<h2 id="add-new-user">Manual Synchronize</h2>
 
-  <?php ?>
-  <table class="form-table">      
-      <td>
-      <span>Manual Synchronization:</span> <br /> 
-      <?php $sync_confirm_url = get_site_url() ."/wp-admin/admin.php?&action=confirm&page=civi_member_sync/manual_sync.php"; ?>
-	      <?php $sync_import_url = get_site_url() ."/wp-admin/admin.php?&action=import&page=civi_member_sync/manual_sync.php"; ?>
-       <input class="button-primary" type="submit" value="Synchronize CiviMember Membership Types to WordPress Roles now" onclick="window.location.href='<?php echo $sync_confirm_url; ?>'" />
-	      <input class="button-primary" type="submit" value="Import CiviMember Members To Wordpress User List" onclick="window.location.href='<?php echo $sync_import_url; ?>'" />
-      </td> 
-   </tr> 
-  </table> 
+	<?php ?>
+	<table class="form-table">
+		<td>
+			<span>Manual Synchronization:</span> <br/>
+			<?php $sync_confirm_url = get_site_url() . "/wp-admin/admin.php?&action=confirm&page=civi_member_sync/manual_sync.php"; ?>
+			<?php $sync_import_url = get_site_url() . "/wp-admin/admin.php?&action=import&page=civi_member_sync/manual_sync.php"; ?>
+			<input class="button-primary" type="submit"
+			       value="Synchronize CiviMember Membership Types to WordPress Roles now"
+			       onclick="window.location.href='<?php echo $sync_confirm_url; ?>'"/>
+			<input class="button-primary" type="submit" value="Import CiviMember Members To Wordpress User List"
+			       onclick="window.location.href='<?php echo $sync_import_url; ?>'"/>
+		</td>
+		</tr>
+	</table>
 </div>
 <?php
 
 require_once( 'civi.php' );
 require_once( ABSPATH . 'wp-content/plugins/civicrm/civicrm/CRM/Core/BAO/UFMatch.php' );
 
-if( isset($_GET['action']) ){
-	if( $_GET['action'] == 'confirm' ) {
-
+if ( isset( $_GET['action'] ) ) {
+	if ( $_GET['action'] == 'confirm' ) {
 
 		$users = get_users();
 
-
 		foreach ( $users as $user ) {
-			$uid = $user->ID;
-			if ( empty( $uid ) ) {
+			$uid    = $user->ID;
+			$uemail = $user->data->user_email;
+
+			if ( empty( $uemail ) ) {
 				continue;
 			}
-			$sql     = "SELECT * FROM civicrm_uf_match WHERE uf_id = $uid";
-			$contact = CRM_Core_DAO::executeQuery( $sql );
 
+			//Mikel -- Updated to use the Civicrm API, and to match CiviUsers with Wordpress users based on primary email
+			//rather than id
 
-			if ( $contact->fetch() ) {
-				$cid        = $contact->contact_id;
+			$contact = civicrm_api3( 'UFMatch', 'get', array(
+				'sequential' => 1,
+				'return'     => array( "uf_id", "contact_id" ),
+				'uf_name'    => $uemail,
+			) );
+
+			if ( isset( $contact ) ) {
+				//if ( $contact->fetch() ) {
+				$cid        = $contact['values']['0']['contact_id'];
 				$memDetails = civicrm_api( "Membership", "get", array(
-					'version'    => '3',
-					'page'       => 'CiviCRM',
-					'q'          => 'civicrm/ajax/rest',
 					'sequential' => '1',
 					'contact_id' => $cid
 				) );
@@ -65,9 +72,10 @@ if( isset($_GET['action']) ){
 
 		?>
 
-	<div id="message" class="updated below-h2">
-		<span><p> CiviMember Memberships and WordPress Roles have been synchronized using available rules. Note: if no association rules exist then synchronization has not been completed.</p></span>
-	</div>
+		<div id="message" class="updated below-h2">
+			<span><p> CiviMember Memberships and WordPress Roles have been synchronized using available rules. Note: if
+					no association rules exist then synchronization has not been completed.</p></span>
+		</div>
 	<?php
 	}
 
@@ -75,56 +83,90 @@ if( isset($_GET['action']) ){
 	//function import_civicrm_members_to_wordpress() {
 
 
-		if ( $_GET['action'] == 'import' ) {
+	if ( $_GET['action'] == 'import' ) {
+
+		//TODO: adjust this so that the targeted member organization(s) and membership types are configurable
+		$result_current_members = civicrm_api3( 'Membership', 'get', array(
+			'sequential'         => 1,
+			'return'             => array( "contact_id" ),
+			'status_id'          => array(
+				'IN' => array(
+					"1",
+					"2",
+					"3"
+				)
+			),
+			'membership_type_id' => array(
+				'IN' => array(
+					"1",
+					"2",
+					"3",
+				)
+			),
+		) );
+
+		$current_member_id_array = array();
+
+		$array_counter = 0;
+		foreach ( $result_current_members['values'] as $key => $values ) {
+
+			$current_member_id_array[ $array_counter ] = $values['contact_id'];
+			$array_counter += 1;
+		}
+
+		$result_member_emails = civicrm_api3( 'Email', 'get', array(
+			'return'     => array( "email", "contact_id" ),
+			'contact_id' => array( 'IN' => $current_member_id_array ),
+			'is_primary' => 1,
+		) );
+
+		if ( isset( $result_member_emails ) ) {
+			$new_account_count = 0;
+
+			foreach ( $result_member_emails['values'] as $key => $values ) {
+
+				//extract the alias part of the email for the new user's username
+				$email    = $values['email'];
+				$parts    = explode( "@", $email );
+				$username = $parts[0];
+
+				$email = $values['email'];
+
+				if ( null == username_exists( $username ) ) {
+					$password = wp_generate_password( 12, true );
+					$user_id  = wp_create_user( $username, $password, $email );
+
+					if ( is_wp_error( $user_id ) ) {
+
+						echo 'Error creating user ' . $username . '/' . $email . ': ' . $user_id->get_error_message();
+						echo '<br>';
+
+					} else {
+
+						echo 'Created account for user ' . $username . ' ID: ' . $user_id . '. <br>';
 
 
-			$result_current_members = civicrm_api3( 'Membership', 'get', array(
-					'sequential' => 1,
-					'return'     => "contact_id",
-					'status_id'  => array(
-						'IN' => array(
-							"1",
-							"3",
-							"2"
-						)
-					),
-				) );
+						wp_update_user(
+							array(
+								'ID'       => $user_id,
+								'nickname' => $email_address,
+							)
 
-			$current_member_id_array = array();
+						);
 
-			$array_counter = 0;
-			foreach ( $result_current_members['values'] as $key => $values ) {
+						// Email the user
+						wp_mail( $email, 'Welcome ' . $username . '!', ' Your Password: ' . $password );
 
-				$current_member_id_array[$array_counter] = $values['contact_id'] ;
-				$array_counter += 1;
-			}
-
-			$result_member_emails = civicrm_api3( 'Email', 'get', array(
-					'return' => array( "email", "contact_id" ),
-					'contact_id' => array( 'IN' => $current_member_id_array ),
-				) );
-
-			if ( isset( $result_member_emails ) ) {
-
-				foreach ( $result_member_emails['values'] as $key => $values ) {
-
-					$email = $values['email'];
-					$parts = explode("@",$email);
-					$username = $parts[0];
-
-					//TODO: create random passwords for the users.
-					//TODO: Make sure that emails are sent to new users with their password and instructions.
-					$user_id = wp_create_user($username,'password',$values['email']);
-
-					echo $values['email'] . ' ID: '  . $user_id . '. <br>';
-
+						$new_account_count += 1;
+					}
 
 				}
 			}
-
-
-
+			echo 'Total new accounts created: ' . $new_account_count;
 		}
+
+
+	}
 	//}
 }?>
 
